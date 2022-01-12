9f2232dfb62d931e1fb8e224ce4aafbeb4cacf77
Y4ROSLAV <jeros1337@gmail.com>
2022-01-12 18:53:18 +0300

    adaptived

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let elements = document.querySelectorAll('.s4__content > div')
-
+let burger = document.querySelector('.header__burger')
+let body = document.querySelector('body')
 
 const showModal = (e) => {
   e.target.querySelector('.modal').classList.add('active')
@@ -9,8 +10,14 @@ const closeModal = (e) => {
   e.target.querySelector('.modal').classList.remove('active')
 }
 
+const toggleBodyOverflow = () => {
+  body.classList.toggle('burger')
+}
+
 for (let elem of elements) {
   elem.addEventListener('mouseenter', (e) => showModal(e))
   elem.addEventListener('mouseleave', (e) => closeModal(e))
 }
+
+burger.addEventListener('click', (e) => toggleBodyOverflow(e))
 
